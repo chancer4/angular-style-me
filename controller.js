99cf8da665c8ac747e6ab6f74429ae90c0e7234a
chancer4 <chance.rhodes5@gmail.com>
2015-11-09 14:56:37 -0500

    Angular Styles
    practiced angularJS

---
+++ controller.js
@@ -0,0 +1,26 @@
+angular.module('myApp', []).controller('myController', function ($scope){
+
+	$scope.items = [
+	{
+		name: "Freddy",
+		desc: "Made a cool weather widget"
+	},
+	{
+		name: "Chance",
+		desc: "Loves Bootstrap"
+	},
+	{
+		name: "Stuart",
+		desc: "Drove a long way"
+	},
+	{
+		name: "will",
+		desc: "tall guy"
+	},
+	{
+		name:"rob",
+		desc:"mastermind"
+	},
+]
+});
+
